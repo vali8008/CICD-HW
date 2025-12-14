f478a69496881560378b49d3b3ed2519c2f7eefa
Walther <walther.miess@endava.com>
2025-12-14 17:57:49 +0200

    Added Terraform plan/apply workflow

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "null" {}
+
+resource "null_resource" "example" {
+  triggers = {
+    example = "hello-from-terraform"
+  }
+}
