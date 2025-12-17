e1a181719727fd3a5264c6d633b6b3c94f2e9a9b
Walther <walther.miess@endava.com>
2025-12-17 10:44:27 +0200

    test7

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.5.0"
-  #test change
   required_providers {
     null = {
       source  = "hashicorp/null"
@@ -17,9 +16,4 @@ resource "null_resource" "example" {
   }
 }
 
-
-
-# testing PR checks
-# PR test check
-#test3 PR workflow
-test PR4
+#test5
